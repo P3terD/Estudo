fd65e7e1dcd13a83249fc77195326c5075315e75
Pietro Lanza <pietrolp333@gmail.com>
2024-04-05 18:28:18 -0300

    :tada: init: first commit including tic-tac-toeand nlw

---
+++ Jogo_da_velha/index.js
@@ -0,0 +1,13 @@
+
+
+function getRegistrationNumber() {
+    //Pega a matrícula do usuário
+}
+
+function getUser() {
+    //Acha o usuário pela matrícula
+}
+
+function calculateGradePointAverage() {
+    //Faz o cálculo das notas do usuário
+}
---
+++ nlw/nlw_unite/mobile/src/app/register.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { colors } from "@/styles/colors"
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
+import { Link } from "expo-router"
+import { View, Image, StatusBar } from "react-native"
+
+export default function Register() {
+    return (
+        <View className="bg-green-500 items-center justify-center flex-1 p-8">
+            <StatusBar barStyle="light-content" />
+
+            <Image
+                source={require("@/assets/logo.png")}
+                className="h-16"
+                resizeMode="contain"
+            />
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <FontAwesome6
+                        name="user-circle"
+                        size={20}
+                        color={colors.green[200]}
+                    />
+                    <Input.Field placeholder="Nome completo" />
+                </Input>
+
+                <Input>
+                    <MaterialIcons
+                        name="alternate-email"
+                        size={20}
+                        color={colors.green[200]}
+                    />
+                    <Input.Field placeholder="E-mail" keyboardType="email-address" />
+                </Input>
+
+                <Button
+                    title="Realizar inscrição"
+                />
+
+                <Link
+                    href={"/"}
+                    className="text-gray-100 text-base font-bold text-center mt-8"
+                >
+                    Já possui ingresso?
+                </Link>
+            </View>
+        </View>
+    )
+}
